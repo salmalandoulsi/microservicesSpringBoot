a6a53be46873c761d534b5e3dc607bc66a174821
slandoulsi <slandoulsi@8YFTPH2>
2020-04-08 10:09:02 +0100

    SecondCommit nom service

--- productService/src/main/java/com/altran/product/controller/ProductController.java
+++ productService/src/main/java/com/altran/product/controller/ProductController.java
@@ -1,10 +1,9 @@
 package com.altran.product.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,53 +28,94 @@ public class ProductController {
 		return "un exemple de produit";
 	}
 
+	
+	/*
+	 * add Product
+	 *  
+	 *  */
 	@PostMapping(value = "/addProduct")
 	public void addProduct(@Valid @RequestBody Product product) {
 
 		productRepository.save(product);
 
 	}
+	
+	/*
+	 * find All Products
+	 *  
+	 *  */
+
 	@GetMapping(value = "/products")
-	public Iterable<Product> getProducts()
-	{	//return Iterable
+	public Iterable<Product> getProducts() { // return Iterable
 		return productRepository.findAll();
 	}
+
 	
-	@GetMapping(path ="/{idProduct}")
-	public Product getProductByID(@PathVariable Long idProduct)
-	{
-		//findById return Optional
-		return  productRepository.findById(idProduct).get();
+	
+	/*
+	 * find Product ByID
+	 *  
+	 *  */
+	
+	@GetMapping(path = "/product/{idProduct}")
+	public Product getProductByID(@PathVariable Long idProduct) {
+		// findById return Optional
+		return productRepository.findById(idProduct).get();
+	}
+	
+	
+	@GetMapping("/product1/{id}")
+	 public ResponseEntity < ? > getProductByID_ResponseEntity(@PathVariable Long id) {
+		try {
+			
+			 return ResponseEntity.ok(getProductByID(id));
+		} catch (Exception e) {
+			   return ResponseEntity.badRequest().body("Invalid product Id");
+			 
+		}
 	}
 	
 	
 	
-	@PutMapping(path ="/{idProduct}" )
-	public void updateProduct(@PathVariable Long idProduct)
-	{
-		Product produt=getProductByID(idProduct);
+	
+	/*
+	 * update Product
+	 *  
+	 *  */
+
+	@PutMapping(path = "/{idProduct}")
+	public void updateProduct(@PathVariable Long idProduct) {
+		Product produt = getProductByID(idProduct);
 		System.out.println(produt.getName());
 		produt.setPrice(200f);
 		productRepository.save(produt);
-		
-	}
-	
-	@PutMapping(value = "/update/{idProduct}" )
-	public void updateProduct(@PathVariable Long idProduct ,@RequestBody Product newproduct)
-	{
-		Product product=getProductByID(idProduct);
-		product=newproduct;
-		product.setId(idProduct);
-		productRepository.save(product);
-		
-	}
-	
-	@DeleteMapping(value="/delete/{idProduct}")
-	public String deleteProduct(@PathVariable Long idProduct)
-	{
-		productRepository.delete(getProductByID(idProduct));
-		return "ok";
-		
+
 	}
 
-}
+	@PutMapping(value = "/update/{idProduct}")
+	public void updateProduct(@PathVariable Long idProduct, @RequestBody Product newproduct) {
+		Product product = getProductByID(idProduct);
+		product = newproduct;
+		product.setId(idProduct);
+		productRepository.save(product);
+
+	}
+	
+	/*
+	 * delete Product
+	 *  
+	 *  */
+
+	@DeleteMapping(value = "/delete/{idProduct}")
+	public String deleteProduct(@PathVariable Long idProduct) {
+		productRepository.delete(getProductByID(idProduct));
+		return "delete done";
+
+	}
+
+	
+
+	
+
+	 }
+
